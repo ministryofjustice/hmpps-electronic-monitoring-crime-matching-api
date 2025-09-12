f8d6bde8f774a184e1df3b3f211e079f5c9d2c3d
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2025-09-12 09:26:55 +0100

    Implement a custom postgres cache manager (#22)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/ElectronicMonitoringCrimeMatchingApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/ElectronicMonitoringCrimeMatchingApi.kt
@@ -2,8 +2,10 @@ package uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 class ElectronicMonitoringCrimeMatchingApi
 
 fun main(args: Array<String>) {
