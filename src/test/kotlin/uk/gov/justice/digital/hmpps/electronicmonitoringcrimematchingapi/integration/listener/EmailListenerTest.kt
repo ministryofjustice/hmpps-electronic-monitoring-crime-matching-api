1c302393fb61425d64472876019120f1b74d7ca0
mitchell-p-gov <mitchell.pearson@justice.gov.uk>
2025-10-27 12:51:08 +0000

    Updating integration tests to use sqs message sending and receiving.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/integration/listener/EmailListenerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/integration/listener/EmailListenerTest.kt
@@ -1,14 +1,14 @@
 package uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.integration.listener
 
-import jakarta.validation.ValidationException
 import org.assertj.core.api.Assertions.assertThat
+import org.awaitility.Awaitility.await
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.io.ClassPathResource
 import org.springframework.test.context.ActiveProfiles
@@ -20,12 +20,19 @@ import software.amazon.awssdk.services.s3.model.DeleteBucketRequest
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest
 import software.amazon.awssdk.services.s3.model.ListObjectsRequest
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import software.amazon.awssdk.services.sqs.SqsAsyncClient
+import software.amazon.awssdk.services.sqs.model.PurgeQueueRequest
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest
+import software.amazon.awssdk.services.sqs.model.SendMessageResponse
 import uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.integration.IntegrationTestBase
-import uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.model.SqsMessage
 import uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.repository.crimeBatch.CrimeBatchRepository
 import uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.repository.crimeBatch.CrimeRepository
-import uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.service.internal.EmailListener
-import java.util.UUID
+import uk.gov.justice.hmpps.sqs.HmppsQueueService
+import uk.gov.justice.hmpps.sqs.MissingQueueException
+import uk.gov.justice.hmpps.sqs.countAllMessagesOnQueue
+import java.util.concurrent.CompletableFuture
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("integration")
@@ -40,7 +47,7 @@ class EmailListenerTest : IntegrationTestBase() {
   lateinit var s3Client: S3Client
 
   @Autowired
-  lateinit var emailListener: EmailListener
+  lateinit var hmppsQueueService: HmppsQueueService
 
   @MockitoSpyBean
   lateinit var crimeBatchRepository: CrimeBatchRepository
@@ -48,9 +55,28 @@ class EmailListenerTest : IntegrationTestBase() {
   @MockitoSpyBean
   lateinit var crimeRepository: CrimeRepository
 
+  val emailQueueConfig by lazy {
+    hmppsQueueService.findByQueueId("email")
+      ?: throw MissingQueueException("HmppsQueue email not found")
+  }
+  val emailQueueSqsUrl by lazy { emailQueueConfig.queueUrl }
+  val emailQueueSqsClient by lazy { emailQueueConfig.sqsClient }
+  val emailDeadLetterSqsClient by lazy { emailQueueConfig.sqsDlqClient as SqsAsyncClient }
+  val emailDeadLetterSqsUrl by lazy { emailQueueConfig.dlqUrl as String }
+
   @BeforeAll
   fun beforeAll() {
     s3Client.createBucket(CreateBucketRequest.builder().bucket(BUCKET_NAME).build())
+  }
+
+  @BeforeEach
+  fun beforeEach() {
+    emailQueueSqsClient.purgeQueue(
+      PurgeQueueRequest.builder().queueUrl(emailQueueSqsUrl).build(),
+    ).get()
+    emailDeadLetterSqsClient.purgeQueue(
+      PurgeQueueRequest.builder().queueUrl(emailDeadLetterSqsUrl).build(),
+    ).get()
   }
 
   @AfterAll
@@ -75,43 +101,63 @@ class EmailListenerTest : IntegrationTestBase() {
     fun `it should process a valid email notification`() {
       val message = """
         {
-          "receipt" : {
-            "action" : {
-              "bucketName" : "$BUCKET_NAME",
-              "objectKey" : "$OBJECT_KEY"
-            }
-          }
+          "Type" : "Notification",
+          "MessageId" : "4730435b-88b9-5b6c-a91c-9b1236b456f7",
+          "TopicArn" : "arn:aws:sns:eu-west-2:000000000000:email-topic",
+          "Message" : "{ \"notificationType\": \"Received\", \"receipt\": { \"action\": { \"bucketName\": \"$BUCKET_NAME\", \"objectKey\": \"$OBJECT_KEY\" }}}"
         }
       """.trimIndent()
       val fileData = ClassPathResource("emailExamples/$OBJECT_KEY")
-      val sqsMessage = SqsMessage("Notification", message, UUID.randomUUID())
 
       s3Client.putObject(PutObjectRequest.builder().bucket(BUCKET_NAME).key(OBJECT_KEY).build(), RequestBody.fromFile(fileData.file))
 
-      emailListener.receiveEmailNotification(sqsMessage)
-      val crimeBatch = crimeBatchRepository.findAll().firstOrNull()
-      assertThat(crimeBatch).isNotNull()
+      sendDomainSqsMessage(message)
+
+      await().until { getNumberOfMessagesCurrentlyOnQueue() == 0 }
+
+      val crimeBatches = crimeBatchRepository.findAll()
+      assertThat(crimeBatches).hasSize(1)
+      assertThat(crimeBatches.first()).isNotNull()
       val crimes = crimeRepository.findAll()
       assertThat(crimes).isNotEmpty()
       assertThat(crimes).hasSize(3)
     }
 
     @Test
-    fun `it should throw a ValidationException when the message content is missing s3 details`() {
+    fun `it should move the message to the dead letter queue when the message content is missing s3 object details`() {
       val message = """
         {
-          "receipt" : {
-            "action" : {
-              "bucketName" : "$BUCKET_NAME"
-            }
-          }
+          "Type" : "Notification",
+          "MessageId" : "4730435b-88b9-5b6c-a91c-9b1236b456f7",
+          "TopicArn" : "arn:aws:sns:eu-west-2:000000000000:email-topic",
+          "Message" : "{ \"notificationType\": \"Received\", \"receipt\": { \"action\": { \"bucketName\": \"$BUCKET_NAME\" }}}"
         }
       """.trimIndent()
-      val sqsMessage = SqsMessage("Notification", message, UUID.randomUUID())
 
-      assertThrows<ValidationException> {
-        emailListener.receiveEmailNotification(sqsMessage)
-      }
+      sendDomainSqsMessage(message)
+
+      await().until { getNumberOfMessagesCurrentlyOnDeadLetterQueue() == 1 }
+
+      val dlqMessage = getMessagesCurrentlyOnDeadLetterQueue().messages().first()
+      assertThat(dlqMessage.body()).isEqualTo(message)
     }
+
+    fun sendDomainSqsMessage(rawMessage: String): CompletableFuture<SendMessageResponse> = emailQueueSqsClient.sendMessage(
+      SendMessageRequest.builder().queueUrl(
+        emailQueueSqsUrl,
+      ).messageBody(rawMessage).build(),
+    )
+
+    fun getNumberOfMessagesCurrentlyOnQueue(): Int = emailQueueSqsClient.countAllMessagesOnQueue(
+      emailQueueSqsUrl,
+    ).get()
+
+    fun getNumberOfMessagesCurrentlyOnDeadLetterQueue(): Int = emailDeadLetterSqsClient.countAllMessagesOnQueue(
+      emailDeadLetterSqsUrl,
+    ).get()
+
+    fun getMessagesCurrentlyOnDeadLetterQueue(): ReceiveMessageResponse = emailDeadLetterSqsClient.receiveMessage(
+      ReceiveMessageRequest.builder().queueUrl(emailDeadLetterSqsUrl).build(),
+    ).get()
   }
 }
