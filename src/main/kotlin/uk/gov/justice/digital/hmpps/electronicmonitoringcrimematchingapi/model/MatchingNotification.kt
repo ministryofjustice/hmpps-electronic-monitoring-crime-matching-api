d1e77d7d2af8d7d8cf3fc8cbe7b4e2e1fb0689bb
Ben Carter <ben.carter@justice.gov.uk>
2025-11-18 15:07:25 +0000

    Format message correctly

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/model/MatchingNotification.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/model/MatchingNotification.kt
@@ -1,6 +1,10 @@
 package uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class MatchingNotification(
   val type: String,
+
+  @JsonProperty("crime_batch_id")
   val crimeBatchId: String,
 )
