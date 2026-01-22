f223eae4ad2273c3384dd52938bb3add70959694
Patrick O'Donnell <patrick.odonnell@justice.gov.uk>
2026-01-22 13:31:57 +0000

    Added non null fields and cascade deletes

--- src/main/resources/db/migration/V3__crime_matching_tables.sql
+++ src/main/resources/db/migration/V3__crime_matching_tables.sql
@@ -3,7 +3,7 @@ CREATE TABLE crime_matching_result
     id                    UUID NOT NULL,
     crime_version_id      UUID NOT NULL,
     crime_matching_run_id UUID NOT NULL,
-    created_at            TIMESTAMP WITHOUT TIME ZONE,
+    created_at            TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     CONSTRAINT pk_crime_matching_result PRIMARY KEY (id)
 );
 
@@ -12,8 +12,8 @@ CREATE TABLE crime_matching_result_device_wearer
     id                       UUID   NOT NULL,
     crime_matching_result_id UUID   NOT NULL,
     device_id                BIGINT NOT NULL,
-    name                     VARCHAR(255),
-    nomis_id                 VARCHAR(255),
+    name                     VARCHAR(255) NOT NULL,
+    nomis_id                 VARCHAR(255) NOT NULL,
     CONSTRAINT pk_crime_matching_result_device_wearer PRIMARY KEY (id)
 );
 
@@ -23,8 +23,8 @@ CREATE TABLE crime_matching_result_position
     crime_matching_result_device_wearer_id UUID             NOT NULL,
     latitude                               DOUBLE PRECISION NOT NULL,
     longitude                              DOUBLE PRECISION NOT NULL,
-    captured_date_time                     TIMESTAMP WITHOUT TIME ZONE,
-    sequence_label                         VARCHAR(255),
+    captured_date_time                     TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    sequence_label                         VARCHAR(255) NOT NULL,
     confidence_circle                      INTEGER          NOT NULL,
     CONSTRAINT pk_crime_matching_result_position PRIMARY KEY (id)
 );
@@ -33,12 +33,12 @@ CREATE TABLE crime_matching_run
 (
     id                UUID NOT NULL,
     crime_batch_id    UUID NOT NULL,
-    algorithm_version VARCHAR(255),
-    trigger_type      VARCHAR(255),
-    status            VARCHAR(255),
-    created_at        TIMESTAMP WITHOUT TIME ZONE,
-    matching_started  TIMESTAMP WITHOUT TIME ZONE,
-    matching_ended    TIMESTAMP WITHOUT TIME ZONE,
+    algorithm_version VARCHAR(255) NOT NULL,
+    trigger_type      VARCHAR(255) NOT NULL,
+    status            VARCHAR(255) NOT NULL,
+    created_at        TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    matching_started  TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    matching_ended    TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     CONSTRAINT pk_crime_matching_run PRIMARY KEY (id)
 );
 
@@ -49,16 +49,21 @@ ALTER TABLE crime_matching_result_device_wearer
     ADD CONSTRAINT uc_crime_matching_result_id_device_id UNIQUE (crime_matching_result_id, device_id);
 
 ALTER TABLE crime_matching_result_position
-    ADD CONSTRAINT FK_CRIMEMATCHINGRESULTPOSITION_ON_CRIMEMATCHINGRESULTDEVICEWEAR FOREIGN KEY (crime_matching_result_device_wearer_id) REFERENCES crime_matching_result_device_wearer (id);
+    ADD CONSTRAINT FK_CRIMEMATCHINGRESULTPOSITION_ON_CRIMEMATCHINGRESULTDEVICEWEAR FOREIGN KEY (crime_matching_result_device_wearer_id) REFERENCES crime_matching_result_device_wearer (id)
+        ON DELETE CASCADE;
 
 ALTER TABLE crime_matching_result_device_wearer
-    ADD CONSTRAINT FK_CRIME_MATCHING_RESULT_DEVICE_WEARER_ON_CRIME_MATCHING_RESULT FOREIGN KEY (crime_matching_result_id) REFERENCES crime_matching_result (id);
+    ADD CONSTRAINT FK_CRIME_MATCHING_RESULT_DEVICE_WEARER_ON_CRIME_MATCHING_RESULT FOREIGN KEY (crime_matching_result_id) REFERENCES crime_matching_result (id)
+        ON DELETE CASCADE;
 
 ALTER TABLE crime_matching_result
-    ADD CONSTRAINT FK_CRIME_MATCHING_RESULT_ON_CRIME_MATCHING_RUN FOREIGN KEY (crime_matching_run_id) REFERENCES crime_matching_run (id);
+    ADD CONSTRAINT FK_CRIME_MATCHING_RESULT_ON_CRIME_MATCHING_RUN FOREIGN KEY (crime_matching_run_id) REFERENCES crime_matching_run (id)
+        ON DELETE CASCADE;
 
 ALTER TABLE crime_matching_result
-    ADD CONSTRAINT FK_CRIME_MATCHING_RESULT_ON_CRIME_VERSION FOREIGN KEY (crime_version_id) REFERENCES crime_version (id);
+    ADD CONSTRAINT FK_CRIME_MATCHING_RESULT_ON_CRIME_VERSION FOREIGN KEY (crime_version_id) REFERENCES crime_version (id)
+        ON DELETE CASCADE;
 
 ALTER TABLE crime_matching_run
-    ADD CONSTRAINT FK_CRIME_MATCHING_RUN_ON_CRIME_BATCH FOREIGN KEY (crime_batch_id) REFERENCES crime_batch (id);
+    ADD CONSTRAINT FK_CRIME_MATCHING_RUN_ON_CRIME_BATCH FOREIGN KEY (crime_batch_id) REFERENCES crime_batch (id)
+        ON DELETE CASCADE;
