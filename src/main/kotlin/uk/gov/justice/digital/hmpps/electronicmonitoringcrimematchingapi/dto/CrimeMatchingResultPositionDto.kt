f56358c1609c8bd26360dd5acf8351953dd62d55
Patrick ODonnell <patrick.odonnell@justice.gov.uk>
2026-01-22 17:15:29 +0000

    Crime matching dtos repo (#64)
    * Add CrimeMatchingRun repository
    
    * Added Crime Matching DTOs

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/dto/CrimeMatchingResultPositionDto.kt
@@ -0,0 +1,22 @@
+package uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.dto
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import java.time.LocalDateTime
+
+data class CrimeMatchingResultPositionDto(
+  @field:NotNull(message = "latitude is required")
+  val latitude: Double,
+
+  @field:NotNull(message = "longitude is required")
+  val longitude: Double,
+
+  @field:NotNull(message = "capturedDateTime is required")
+  val capturedDateTime: LocalDateTime,
+
+  @field:NotBlank(message = "sequenceLabel is required")
+  val sequenceLabel: String,
+
+  @field:NotNull(message = "confidenceCircle is required")
+  val confidenceCircle: Int,
+)
