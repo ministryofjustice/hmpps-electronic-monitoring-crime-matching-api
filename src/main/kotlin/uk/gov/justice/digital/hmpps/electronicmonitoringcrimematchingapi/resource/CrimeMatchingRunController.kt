8382701f45a7721ddc5715d4d8316fc3cef4062b
Patrick O'Donnell <patrick.odonnell@justice.gov.uk>
2026-01-23 14:48:02 +0000

    Added Crime Matching Run Controller with "/crime-matching-run" endpoint

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/resource/CrimeMatchingRunController.kt
@@ -0,0 +1,44 @@
+package uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.resource
+
+import io.swagger.v3.oas.annotations.Operation
+import jakarta.validation.Valid
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.dto.CrimeMatchingRunCreatedDto
+import uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.dto.CrimeMatchingRunDto
+import uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.dto.ResponseDto
+import uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.service.crimeMatching.CrimeMatchingRunService
+
+@RestController
+// TODO: Create role for this - @PreAuthorize("hasAnyAuthority('ROLE_EM_CRIME_MATCHING__CRIME_MATCHING_RESULTS__RW')")
+@RequestMapping("/crime-matching-run", produces = ["application/json"])
+class CrimeMatchingRunController(
+  private val crimeMatchingRunService: CrimeMatchingRunService,
+) {
+  @Operation(
+    tags = ["Crime Matching"],
+    summary = "Create a crime matching run",
+  )
+  @RequestMapping(
+    method = [RequestMethod.POST],
+    consumes = [MediaType.APPLICATION_JSON_VALUE],
+    produces = [MediaType.APPLICATION_JSON_VALUE],
+  )
+  fun createCrimeMatchingRun(
+    @Valid @RequestBody runDto: CrimeMatchingRunDto,
+  ): ResponseEntity<ResponseDto<CrimeMatchingRunCreatedDto>> {
+    val run = crimeMatchingRunService.createCrimeMatchingRun(runDto)
+
+    return ResponseEntity.status(201).body(
+      ResponseDto(
+        CrimeMatchingRunCreatedDto(
+          id = run.id.toString(),
+        ),
+      ),
+    )
+  }
+}
