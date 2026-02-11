a39678241b0ba2c9052e266450a99b229ac10d2e
Ben Carter <ben.carter@justice.gov.uk>
2026-02-11 11:00:36 +0000

    Use mappers

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/dto/CrimeBatchDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/dto/CrimeBatchDto.kt
@@ -1,17 +1,7 @@
 package uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.dto
 
-import uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.model.entity.CrimeBatch
-
 data class CrimeBatchDto(
   val id: String,
   val batchId: String,
   val crimes: List<CrimeDto>,
-) {
-  constructor(crimeBatch: CrimeBatch) : this(
-    id = crimeBatch.id.toString(),
-    batchId = crimeBatch.batchId,
-    crimes = crimeBatch.crimeVersions.map {
-      CrimeDto(it)
-    },
-  )
-}
+)
