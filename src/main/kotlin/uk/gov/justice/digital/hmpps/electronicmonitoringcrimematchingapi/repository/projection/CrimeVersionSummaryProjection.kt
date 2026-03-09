2c1fa7fab2ac874a2f5f6925ced97065bb9454b2
Patrick ODonnell <patrick.odonnell@justice.gov.uk>
2026-03-09 13:38:40 +0000

    Acr 567 return crime version summaries (#101)
    * Happy path API working
    
    * Handle empty crimeRef param
    
    * Ran kotlin formatter

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/repository/projection/CrimeVersionSummaryProjection.kt
@@ -0,0 +1,16 @@
+package uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.repository.projection
+
+import java.time.LocalDateTime
+
+interface CrimeVersionSummaryProjection {
+  val crimeVersionId: String
+  val crimeReference: String
+  val policeForceArea: String
+  val crimeTypeId: String
+  val crimeDateTimeFrom: LocalDateTime
+  val batchId: String
+  val ingestionDateTime: LocalDateTime
+  val matched: Boolean
+  val versionLabel: String
+  val updates: String
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/resource/CrimeVersionController.kt
@@ -0,0 +1,73 @@
+package uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.resource
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+import uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.dto.CrimeVersionSummaryResponse
+import uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.dto.PagedResponse
+import uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.mappers.CrimeVersionSummaryMapper
+import uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.service.proximityAlert.CrimeVersionService
+
+@RestController
+@PreAuthorize("hasAnyAuthority('ROLE_EM_CRIME_MATCHING_GENERAL_RO')")
+@RequestMapping("/crime-versions", produces = ["application/json"])
+class CrimeVersionController(
+  private val crimeVersionService: CrimeVersionService,
+  private val crimeVersionSummaryMapper: CrimeVersionSummaryMapper,
+) {
+
+  @Operation(
+    tags = ["Crime Versions"],
+    summary = "Search crime versions by crime reference",
+  )
+  @GetMapping
+  fun searchCrimeVersions(
+    @Parameter(
+      description = "Crime reference prefix to search for",
+      required = true,
+    )
+    @RequestParam
+    crimeRef: String,
+    @Parameter(
+      description = "Page number (0-based)",
+      example = "0",
+    )
+    @RequestParam(defaultValue = "0")
+    page: Int,
+    @Parameter(
+      description = "Number of items per page",
+      example = "30",
+    )
+    @RequestParam(defaultValue = "30")
+    pageSize: Int,
+  ): ResponseEntity<PagedResponse<CrimeVersionSummaryResponse>> {
+    if (crimeRef.isBlank()) {
+      throw ResponseStatusException(
+        HttpStatus.BAD_REQUEST,
+        "crimeRef must not be blank",
+      )
+    }
+
+    val results = crimeVersionService.searchCrimeVersions(
+      crimeRef,
+      page,
+      pageSize,
+    )
+
+    return ResponseEntity.ok(
+      PagedResponse(
+        results.content.map { crimeVersionSummaryMapper.toDto(it) },
+        results.totalPages,
+        results.number,
+        results.size,
+      ),
+    )
+  }
+}
