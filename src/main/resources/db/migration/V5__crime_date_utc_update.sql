f2a594a75c6cf2ff62f8904e1d31628f88609952
mitchell-p-gov <mitchell.pearson@justice.gov.uk>
2026-03-16 11:27:41 +0000

    Updating crimeDateTime column types to TIMESTAMPTZ to ensure instants are stored and retrieved in UTC format and removing the enforce jdbc timezone

--- src/main/resources/db/migration/V5__crime_date_utc_update.sql
+++ src/main/resources/db/migration/V5__crime_date_utc_update.sql
@@ -1,9 +1,9 @@
 ALTER TABLE crime_version
     ALTER COLUMN crime_date_time_from
-    TYPE TIMESTAMP
+    TYPE TIMESTAMPTZ
     USING crime_date_time_from AT TIME ZONE 'UTC';
 
 ALTER TABLE crime_version
     ALTER COLUMN crime_date_time_to
-    TYPE TIMESTAMP
+    TYPE TIMESTAMPTZ
     USING crime_date_time_to AT TIME ZONE 'UTC';
