9600e0d83d90035e10da9556dbe62be792e19d35
Ben Carter <ben.carter@justice.gov.uk>
2026-06-19 10:20:34 +0100

    Only use the athena endpoint url for athena requests i.e. not STS

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/config/aws/AthenaProperties.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/config/aws/AthenaProperties.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.config.aws
 
 data class AthenaProperties(
+  val endpointUrl: String? = null,
   val role: String? = null,
 )
