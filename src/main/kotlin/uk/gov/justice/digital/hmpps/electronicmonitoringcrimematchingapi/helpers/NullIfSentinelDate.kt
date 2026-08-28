ed6deaa1dac1d9ce68c147abd261e84bc78b56b7
mitchell-p-gov <mitchell.pearson@justice.gov.uk>
2026-08-28 10:03:24 +0100

    Updating sentinel date value to 9999-12-31 00:00:00.000 and updating grafana dashboard links.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/helpers/NullIfSentinelDate.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/helpers/NullIfSentinelDate.kt
@@ -2,7 +2,7 @@ package uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.helper
 
 import java.time.LocalDateTime
 
-private val DEVICE_DEACTIVATION_DATE_SENTINEL = LocalDateTime.parse("2999-12-12 23:59:59.000", formatter)
+private val DEVICE_DEACTIVATION_DATE_SENTINEL = LocalDateTime.parse("9999-12-31 00:00:00.000", formatter)
 
 fun nullIfSentinelDate(value: LocalDateTime?): LocalDateTime? = value
   ?.takeUnless { it == DEVICE_DEACTIVATION_DATE_SENTINEL }
