63e1df9c2e9bd8e5622ff4bca33f1054ad712ba3
Matthew Smith Huang <matthew.smithhuang@justice.gov.uk>
2026-09-23 15:37:28 +0100

    ACR-980: Add retry logic to Gov Notify email outbox
    Max retries hardcoded to three. DEAD state used to indicate that a retry is unlikely to succeed

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/model/enums/EmailOutboxState.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/model/enums/EmailOutboxState.kt
@@ -3,6 +3,6 @@ package uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.model.
 enum class EmailOutboxState {
   PENDING, // About to attempt to publish (it's possible that we have published but were unable to record it)
   PUBLISHED, // Definitely published, but we don't know what happened downstream.
-  FAILED, // Failed but we plan to retry (not yet implemented - currently is a terminal state)
-  DEAD, // Failed and we don't plan to retry (not yet implemented)
+  FAILED, // Failed but we plan to retry
+  DEAD, // Failed and we don't plan to retry
 }
