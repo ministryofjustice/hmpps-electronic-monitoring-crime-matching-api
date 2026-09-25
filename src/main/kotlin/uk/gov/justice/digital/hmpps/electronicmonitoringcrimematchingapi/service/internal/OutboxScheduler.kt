f4494b950806fa3286ef90d0d9b9af158344d03f
Matthew Smith-Huang <matthew.smithhuang@justice.gov.uk>
2026-09-25 10:25:39 +0100

    ACR-980: Add scheduler for the email outbox. Offset slightly (one minute) from the publish matching outbox scheduler (#260)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/service/internal/OutboxScheduler.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringcrimematchingapi/service/internal/OutboxScheduler.kt
@@ -5,15 +5,22 @@ import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.electronicmonitoringcrimematchingapi.service.MatchingNotificationService
 
 @Service
-class MatchingNotificationScheduler(
+class OutboxScheduler(
   private val matchingNotificationService: MatchingNotificationService,
+  private val emailNotificationService: EmailNotificationService,
 ) {
   companion object {
     const val EVERY_15_MINUTES: String = "PT15M"
+    const val ONE_MINUTE: String = "PT1M"
   }
 
   @Scheduled(fixedRateString = EVERY_15_MINUTES)
   fun publishMatchingRequestsScheduled() {
     matchingNotificationService.publishMatchingRequests()
+  }
+
+  @Scheduled(fixedRateString = EVERY_15_MINUTES, initialDelayString = ONE_MINUTE)
+  fun notifyEmailRequestsScheduled() {
+    emailNotificationService.sendEmails()
   }
 }
